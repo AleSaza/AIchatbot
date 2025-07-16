61c3ab22cf3b476101c852b320d08fb0869553cd
alesaza <aperreira495@gmail.com>
2025-07-16 12:59:56 -0300

    Estrutura base do meu projeto

---
+++ zap.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+import time
+import os
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+dir_path = os.getcwd()
+chrome_options2 = Options()
+chrome_options2.add_argument(r"user-data-dir=" + dir_path + "profile/zap")
+driver = webdriver.Chrome(options=chrome_options2)
+driver.get('https://web.whatsapp.com/')
+time.sleep(120)
